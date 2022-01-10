e645d57c41be75a646b72941c47032d6159b8868
Kim Palao <kimfarhantpalao@gmail.com>
2022-01-10 17:16:10 +0800

    Add command for running all tests in a file

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
+let lastCommand: string;
+
+// this method is called when your extension is activated 
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "laratest" is now active!');
+  context.subscriptions.push(vscode.commands.registerCommand('laratest.runFileTests', async () => {
+    lastCommand = vscode.window.activeTextEditor?.document.fileName ?? '';
+    await vscode.commands.executeCommand('workbench.action.terminal.clear');
+    await vscode.commands.executeCommand('workbench.action.tasks.runTask', `laratest: run`);
+  }));
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('laratest.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World VS Code!');
-  });
+  context.subscriptions.push(vscode.tasks.registerTaskProvider('laratest', {
+    provideTasks: () => {
+      return [new vscode.Task(
+        { type: 'laratest', task: 'run' },
+        vscode.TaskScope.Workspace,
+        'run',
+        'laratest',
+        new vscode.ShellExecution(`php artisan test ${lastCommand}`),
+        '$laratest'
+      )];
+    },
+    resolveTask: () => {
+      return undefined;
+    }
+  }));
 
-  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
